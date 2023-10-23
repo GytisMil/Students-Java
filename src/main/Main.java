3b5d53c2678e6571f3c00d83b96d6db51308e4ee
GytisMil <gytis.miliauskas963@gmail.com>
2023-10-23 11:00:00 +0300

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		//Hello World
+		System.out.println("My name Jeff");
+	}
+}
---
+++ src/main/package-info.java
@@ -0,0 +1,7 @@
+package main;
+/* TODO: Student generation and read
+Calculate average
+Human -> Student classes
+Assign group to every student by average
+Output new list of Students
+*/
